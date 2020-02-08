91cca64b54fcb56281019cd82457205b777282ab
san2157895 <60497607+san2157895@users.noreply.github.com>
2020-02-07 23:54:40 -0700

    Add files via upload

---
+++ variables.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" 
+<head>
+  <meta charset="utf-8">
+  <title>Variables</title>
+</head>
+<body>
+<?php // Variables
+$first = "Santiago";
+$last = "Alvarado";
+$address = "32nd Street, Phoenix AZ 85017";
+$phone = "(602) 123-4567";
+
+//Print
+print "<p>$first $last<br>$address<br>$phone</p>"
+
+?>
+ 
+</body>
+</html>
